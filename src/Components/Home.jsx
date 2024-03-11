f54f95f5b5e555402408030fa6ad9d1bd85f7aba
jvittor <102699222+jvittor@users.noreply.github.com>
2024-03-11 15:34:02 -0300

    :sparkles:  feat: homepage [adicionando os textos e logo nos grid's header e nav]"

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Box, Grid, GridItem } from "@chakra-ui/react";
+import { Box, Grid, GridItem, Image, Text } from "@chakra-ui/react";
+import LogoCompany from "../assets/logo.png"
 
 const Navbar = () => {
   return (
@@ -8,24 +9,69 @@ const Navbar = () => {
                     "nav main"
                     "nav footer"`}
     gridTemplateRows={'50px 1fr 30px'}
+    
     gridTemplateColumns={'300px 1fr'}
     h='100vh'
-    gap='1'
-    color='blackAlpha.700'
+    gap='0'
+    color='black'
     fontWeight='bold'
   >
-    <GridItem pl='2' bg='orange.300' area={'header'}>
-      HeaderNavbar
+    <GridItem pl='4' bg='#424242' area={'header'}>
+      <Text
+      fontFamily={"Fira Code"}
+      fontSize={"20px"}
+      color={"white"}
+      pt={2.5}
+      
+      >
+        gepetto
+      </Text>
     </GridItem>
-    <GridItem pl='4' bg='pink.300' area={'nav'} height={'full'}>
-      Nav
+    <GridItem bg='#121212' area={'nav'} height={'full'}>
+      <Box
+        bg="#121212"
+        w={"100%"}
+        h={"50px"}
+        mt={2}
+        as="button"
+        _hover={{
+          bg: "#444D55", 
+        }}
+        >
+          <Text
+          fontFamily={"Fira Code"}
+          fontSize={"16px"}
+          pr={5}
+          color={"white"}
+          fontStyle={"regular"}
+          letterSpacing={"0.15px"}
+          >Pagamento</Text>
+        </Box>
+        <Box
+        g="#121212"
+        w={"100%"}
+        h={"50px"}
+        mt={2}
+        as="button"
+        _hover={{
+          bg: "#444D55", 
+        }}
+        >
+          <Text
+          fontFamily={"Fira Code"}
+          fontSize={"16px"}
+          pr={5}
+          color={"white"}
+          >
+            Saldo
+          </Text>
+        </Box>
     </GridItem>
-    <GridItem pl='2' bg='green.300' area={'main'}>
+    <GridItem pl='2' bg='white'>
       Main
     </GridItem>
-    <GridItem pl='2' bg='blue.300' area={'footer'}>
-      Footer
-    </GridItem>
+    <GridItem bg='white' area={'footer'}>
+      </GridItem>
   </Grid>
   );
 };
