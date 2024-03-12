195ed0e0b94c5512671bdda3c9e61936115a3d54
jvittor <102699222+jvittor@users.noreply.github.com>
2024-03-11 21:56:37 -0300

    :bug: fix: Add Rotas de Login, Register e NotFoundPage

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,20 @@ import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home.jsx";
+import Login from "./Components/LoginPage/Login.jsx";
+import Register from "./Components/RegisterPage/Register.jsx";
+import NotFoundPage from "./Components/NotFoundPage.jsx";
 
 const App = () => {
   return (
     <ChakraProvider>
       <BrowserRouter>
         <Routes>
-          {/* Alterar caminho da página de login */}
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/home" element={<Home />} />
+
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </BrowserRouter>
     </ChakraProvider>
