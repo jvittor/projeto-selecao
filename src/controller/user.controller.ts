92bcea79eeaa11dabd6d58ef2647ae6c3f9969e7
jvittor <102699222+jvittor@users.noreply.github.com>
2024-03-15 11:34:39 -0300

    :lock: add: criptografia de senha
    estou utilizando como criptografia de senha a biblioteca "bcrypt.js"

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,14 +1,16 @@
 import { Request, Response } from "express";
 import { prisma } from "../utils/prisma";
+import bcrypt from "bcryptjs";
 
 
 export class UserController {
     async store(req: Request, res: Response) {
         const {email, password} = req.body;
+        const hash_password = await bcrypt.hash(password, 8);
         const user = await prisma.user.create({
             data: {
                 email, 
-                password,
+                password: hash_password,
             },
         });
 
