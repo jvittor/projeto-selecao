e3a5fc083fc69477b41ff522c9ccd4a7c477507c
jvittor <102699222+jvittor@users.noreply.github.com>
2024-03-15 13:40:24 -0300

    :lock: add: protect routes with jwt using middlewares

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import {Router} from 'express'
 import { UserController } from './controller/user.controller'
 import { AuthController } from './controller/auth/auth.controller';
+import { AuthMiddleware } from './middlewares/auth';
+
 
 const userController = new UserController();
 const authController = new AuthController();
@@ -10,4 +12,4 @@ const authController = new AuthController();
 export const router = Router();
 
 router.post("/register", userController.store);
-router.post("/login", authController.authenticate)
+router.post("/login", AuthMiddleware, authController.authenticate)
