1d996f3a82b402956b07cbdc3689351d183282db
jvittor <102699222+jvittor@users.noreply.github.com>
2024-03-16 13:45:53 -0300

    :sparkles: feat: login/register controllers

---
+++ src/controllers/user/user-register.controller.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { prisma } from "../../utils/prisma";
+import bcrypt from "bcryptjs";
+
+
+export class UserRegisterController {
+    async store(req: Request, res: Response) {
+        const {email, password} = req.body;
+        
+        const userExists = await prisma.user.findUnique({ where: { email } });
+        if(userExists){
+            return res.json({ error: "usuario ja existe!"})
+        }
+        const hash_password = await bcrypt.hash(password, 8);
+        const user = await prisma.user.create({
+            data: {
+                email, 
+                password: hash_password,
+            },
+        });
+
+        return res.json({ user })
+    }
+}
