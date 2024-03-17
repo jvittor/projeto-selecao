75fe2e3c058fb009f50bb15145aca73c5a044993
jvittor <102699222+jvittor@users.noreply.github.com>
2024-03-17 00:04:11 -0300

    :recycle: create dinamismo de micro-páginas de criação de pagamentos

--- src/components/payment/PaymentTable.jsx
+++ src/components/payment/PaymentTable.jsx
@@ -14,29 +14,29 @@ import {
   Input,
   IconButton,
   Button,
-  Flex
+  Flex,
+  Grid
 } from "@chakra-ui/react";
 import { ChevronLeftIcon, ChevronRightIcon} from "@chakra-ui/icons";
-import { FiEdit2, FiTrash2 } from "react-icons/fi";
-
-const data = [
-  // test datA
-
-];
+import { FiEdit2 } from "react-icons/fi";
+import PaymentCreate from "./PaymentCreate";
+import mockPayment from "../mocksData/payment/paymentData.json";
 
 const PaymentTable = () => {
-  const [rowsPerPage, setRowsPerPage] = useState(3);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
   const [currentPage, setCurrentPage] = useState(1);
   const [searchTerm, setSearchTerm] = useState("");
-
+  const [showPaymentCreate, setShowPaymentCreate] = useState(false);
 
   const handleChangeRowsPerPage = (event) => {
     setRowsPerPage(parseInt(event.target.value, 10));
     setCurrentPage(1);
   };
 
-  const filteredData = data.filter((item) =>
-    item.nome.toLowerCase().includes(searchTerm.toLowerCase())
+
+  const paymentData = mockPayment.payment || [];
+  const filteredData = paymentData.filter((item) =>
+    item.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
   const totalPages = Math.ceil(filteredData.length / rowsPerPage);
@@ -52,112 +52,124 @@ const PaymentTable = () => {
   };
 
   const handleCreatePedido = () => {
-    // test
+    setShowPaymentCreate(true); 
   }
 
   return (
-    
-    <Stack spacing={4} p={4}>
-      
-      {data.length === 0 ? (
-      <Flex direction="column"
-      align="center"
-      justify="center"
-      height="70vh"
-      >
-        <Button onClick={handleCreatePedido} colorScheme="teal" bg={"#1976D2"} _hover={{ bg: "#444D55" }}>
-          Criar Pedido
-        </Button>
-      </Flex>
-    ) : (
-      <>
-      <Box>
-        <Input
-          placeholder="Pesquisa de pagamento"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-      </Box>
-      <Box overflowX="auto">
-        <Table variant="simple" size="sm">
-          <Thead>
-            <Tr>
-              <Th>Nome</Th>
-              <Th>Descrição</Th>
-              <Th>Valor</Th>
-              <Th>Ação</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {filteredData
-              .slice(startIndex, endIndex)
-              .map((item) => (
-                <Tr key={item.nome}>
-                  <Td>{item.nome}</Td>
-                  <Td>{item.descricao}</Td>
-                  <Td>{item.valor}</Td>
-                  <Td>
-                    <Stack direction="row">
-                      <IconButton
-                        icon={<FiEdit2 />}
-                        aria-label="Editar"
-                        size="sm"
-                      />
-                      <IconButton
-                        icon={<FiTrash2 />}
-                        aria-label="Excluir"
-                        size="sm"
-                      />
-                    </Stack>
-                  </Td>
-                </Tr>
-              ))}
-          </Tbody>
-          <Tfoot>
-            <Tr>
-              <Th colSpan={3} >
-                <Stack direction={["column", "row"]} spacing={2} align={["flex-start", "center"]}>
-                  <label htmlFor="rowsPerPage">
-                    <Text whiteSpace="nowrap">Rows per page: </Text>
-                  </label>
-                    <Select
-                      id="rowsPerPage"
-                      value={rowsPerPage}
-                      onChange={handleChangeRowsPerPage}
-                      size="sm"
-                      w={[20, 32]}
-                      ml={['center', 2]}
-                      align={["flex-start", "center"]}
-                    >
-                      <option value={5}>5</option>
-                      <option value={10}>10</option>
-                      <option value={20}>20</option>
-                    </Select>
-                  <Text ml={[0, 2]} whiteSpace="nowrap">
-                    {`${startIndex + 1}-${endIndex} of ${filteredData.length} `}
-                  </Text>
-                  <Stack direction="row">
-                    <IconButton
-                      icon={<ChevronLeftIcon />}
-                      onClick={handlePrevPage}
-                      isDisabled={currentPage === 1}
-                    />
-                    <IconButton
-                      icon={<ChevronRightIcon />}
-                      onClick={handleNextPage}
-                      isDisabled={currentPage === totalPages}
-                    />
-                  </Stack>
-                </Stack>
-              </Th>
-              <Th display={["none", "table-cell"]}></Th>
-            </Tr>
-          </Tfoot>
-        </Table>
-      </Box>
-      </>
-    )}
-  </Stack>
+    <Grid>
+      {showPaymentCreate ? ( 
+        <PaymentCreate onCancel={() => setShowPaymentCreate(false)} />
+      ) : (
+        <Stack spacing={4} p={4}>
+          <Text mt={5} ml={{ base: 3, md: 5 }} fontSize="24px" fontFamily={"Roboto"} fontStyle={"regular"} letterSpacing={"0.15px"}>Pagamentos</Text>
+          {paymentData.length === 0 ? (
+            <Flex direction="column" align="center" justify="center" height="70vh">
+              <Text mb={4}>Não há pagamentos disponíveis.</Text>
+              <Button onClick={handleCreatePedido} colorScheme="teal" bg={"#1976D2"} _hover={{ bg: "#444D55" }}>
+                Criar Pedido
+              </Button>
+            </Flex>
+          ) : (
+            <>
+              <Button
+                position="absolute"
+                top={20}
+                right={{ base: 10, md: 8 }}
+                colorScheme="teal"
+                bg="#1976D2"
+                _hover={{ bg: "#444D55" }}
+                onClick={handleCreatePedido}
+              >
+                Criar Pedido
+              </Button>
+              <Stack w={{base : "90%", md: "97%"}}>
+                <Input
+                  placeholder="Pesquisa de pagamento"
+                  type="search"
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                  ml={{ base: 3, md: 5 }}
+                />
+              </Stack>
+              <Box>
+                <Table variant="simple" size="50">
+                  <Thead>
+                    <Tr fontSize={{ base: "14px", md: "15px"}}>
+                      <Th>Nome</Th>
+                      <Th >Descrição</Th>
+                      <Th>Valor</Th>
+                      <Th >Ação</Th>
+                    </Tr>
+                  </Thead>
+                  <Tbody> 
+                    {filteredData
+                      .slice(startIndex, endIndex)
+                      .map((item) => (
+                        <Tr key={item.name} fontSize={{ base: "14px", md: "15px"}}>
+                          <Td >{item.name}</Td>
+                          <Td >{item.description}</Td>
+                          <Td>{item.value}</Td>
+                          <Td pl={2}>
+                            <Stack direction="row">
+                              <IconButton
+                                icon={<FiEdit2 />}
+                                aria-label="Editar"
+                                size="sm"
+                              />
+
+                            </Stack>
+                          </Td>
+                        </Tr>
+                      ))}
+                  </Tbody>
+                  <Tfoot>
+                    <Tr>
+                      <Th colSpan={3} fontSize={"15px"} left={5} >
+                        <Stack direction={["column", "row"]} spacing={2} >
+                          <label htmlFor="rowsPerPage">
+                            <Text whiteSpace="nowrap" mt={{base: 0, md: 2}}>Rows per page: </Text>
+                          </label>
+                          <Select
+                            id="rowsPerPage"
+                            value={rowsPerPage}
+                            onChange={handleChangeRowsPerPage}
+                            size="sm"
+                            w={[20, 32]}
+                            ml={['center', 2]}
+                            mt={{base: 0, md: 1}}
+                            align={["flex-start", "center"]}
+                          >
+                            <option value={5}>5</option>
+                            <option value={10}>10</option>
+                            <option value={20}>20</option>
+                          </Select>
+                          <Text whiteSpace="nowrap" direction="row" position={{base: "absolute", md: "relative"}} ml={{ base: "auto", md: 0 }} right={{base: 15, md: 0}} mt={{ base: 0, md: 2 }}>
+                            {`${startIndex + 1}-${endIndex} of ${filteredData.length} `}
+                          </Text>
+                          <Stack direction="row" position={{base: "absolute", md: "relative"}} ml={{ base: "auto", md: 0 }} right={{base: 5, md: 0 }} mt={{ base: 5, md: 0 }}>
+                            <IconButton
+                              icon={<ChevronLeftIcon />}
+                              onClick={handlePrevPage}
+                              isDisabled={currentPage === 1}
+                            />
+                            <IconButton
+                              icon={<ChevronRightIcon />}
+                              onClick={handleNextPage}
+                              isDisabled={currentPage === totalPages}
+                            />
+                          </Stack>
+                        </Stack>
+                      </Th>
+                      <Th display={["none", "table-cell"]}></Th>
+                    </Tr>
+                  </Tfoot>
+                </Table>
+              </Box>
+            </>
+          )}
+        </Stack>
+      )}
+    </Grid>
   );
 };
 
